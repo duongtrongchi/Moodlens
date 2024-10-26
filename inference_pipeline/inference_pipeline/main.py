e16739084ca78156b9f2a0c0c7ba97efb37b5dbd
chiduong <duongchi68389@gmail.com>
2024-10-26 04:45:13 +0000

    update promethues and grafana

--- inference_pipeline/inference_pipeline/main.py
+++ inference_pipeline/inference_pipeline/main.py
@@ -1,11 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from inference_pipeline.api.inference import router
 
 app = FastAPI()
 app.include_router(router)
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -14,3 +27,5 @@ async def root():
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+
+Instrumentator().instrument(app).expose(app)
